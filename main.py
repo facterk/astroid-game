e0b9ea9a760b9e5c1b5871f7335979682a23c9bf
facterk <facterk6969@yahoo.com>
2025-05-06 17:58:21 +0330

    added the constants.py and imported its content to main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+from constants import *
 def main():
     result = "Starting Asteroids!"
     print(result)
     return result
 if __name__ =="__main__":
     main()
+    print(f"Screen width {SCREEN_WIDTH}")
+    print(f"Screen height {SCREEN_HEIGHT}")
