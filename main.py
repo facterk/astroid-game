ac7eda82432107dda6cb567870c53b6575794716
facterk <facterk6969@yahoo.com>
2025-05-08 01:00:53 +0330

    added code to create a black displat screen

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
 from constants import *
+
 def main():
     result = "Starting Asteroids!"
     print(result)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    running = True
+    while running:
+        screen.fill(0,0,0)
+        pygame.display.flip()
     return result
 if __name__ =="__main__":
     main()
