2625592dbf0c0ede7f34e336542e5911d76ce7a9
facterk <facterk6969@yahoo.com>
2025-05-08 04:31:11 +0330

    added code to ensure the game runs at 60 frames per second, and prvent the game from taking too much cpu resorses

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ def main():
     result = "Starting Asteroids!"
     print(result)
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
         screen.fill((0,0,0))
         pygame.display.flip()
