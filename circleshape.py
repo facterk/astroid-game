9fbc58b8d459051d699b37a96921a8fba2ae0c7a
facterk <facterk6969@yahoo.com>
2025-05-09 20:12:28 +0330

    1- created circleshape file with circleshape class to create the player object and set up for the creation of the astroids 2- created player file to make the player object 3- added player radious to constants 4- added the code to main file to create the player body on the screen

---
+++ circleshape.py
@@ -0,0 +1,17 @@
+import pygame
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        self.radius = radius
+        self.position = pygame.Vector2(x,y)
+        self.velocity = pygame.Vector2(0,0)
+        if hasattr(self,"containers"):
+            super().__init__(self,container)
+        else :
+            super().__init__()
+    
+    def draw(self, screen):
+        result = pygame.draw.polygon(screen,"white", self.triangle(), 2)
+        return result
+
+    def update(self,dt):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     result = "Starting Asteroids!"
@@ -9,6 +10,8 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     running = True
+    player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +19,7 @@ def main():
         dt = clock.tick(60) / 1000
 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
     return result
 if __name__ =="__main__":
