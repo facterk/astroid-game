d49e417ebcc58faa86a9512f954769c51f331660
facterk <facterk6969@yahoo.com>
2025-05-12 11:25:38 +0330

    added code to limit the speed at which the gun can fire

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self,x,y,):
         super().__init__(x , y , PLAYER_RADIUS, *Player.containers)
         self.rotation = 0
+        self.shot_timer = 0
 
 
     def triangle(self):
@@ -23,6 +24,11 @@ class Player(CircleShape):
     
 
     def update(self, dt):
+        if self.shot_timer > 0 :
+            self.shot_timer -= dt
+            if self.shot_timer < 0:
+                self.shot_timer = 0
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -46,6 +52,9 @@ class Player(CircleShape):
 
 
     def shoot(self):
+        if self.shot_timer > 0 :
+            return 
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         velocity = pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
         shot.velocity = velocity
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
